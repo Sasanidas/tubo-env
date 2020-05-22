261a0b2ccd68dd58c95b7afb7a5f833a8784db0d
yangyingchao <yangyingchao@gmail.com>
2020-05-22 08:44:49 +0800

    update.

--- .emacs.d/site-lisp/utils/debug-utils.el
+++ .emacs.d/site-lisp/utils/debug-utils.el
@@ -1,4 +1,4 @@
-;;; debug-utils.el -- Brief introduction here.
+;;; debug-utils.el -- Brief introduction here. -*- lexical-binding: t; -*-
 
 ;; Author: Yang,Ying-chao <yingchao.yang@icloud.com>
 
@@ -6,27 +6,62 @@
 
 ;;; Code:
 
+(require '02-functions)
+(require 'cl)
+(require 'realgud)
+(require 's)
+(require 'ivy)
+(require 'swiper)
+
 
 (defun yc/call-gdb (cmd)
   "Call gdb with CMD."
   (PDEBUG
-    "PFX:" current-prefix-arg
-    "GDB ARGS" cmd)
+   "PFX:" current-prefix-arg
+   "GDB ARGS" cmd)
   (if current-prefix-arg
       (gdb (concat "gdb -i=mi "  cmd) )
-    (realgud:gdb (concat "gdb "cmd))
-  ))
+    (realgud:gdb (concat "gdb "cmd))))
 
 (defun yc/directory-files (dir)
-    "List files in DIR except . & .. ."
-    (mapcar (lambda (x)
-              (concat dir x) )
-            (remove-if
-             (lambda (x)
-               (or (equal "." x)
-                   (equal ".." x)))
-             (directory-files dir
-                              nil))))
+  "List files in DIR except . & .. ."
+  (mapcar (lambda (x)
+            (concat dir x) )
+          (remove-if
+           (lambda (x)
+             (or (equal "." x)
+                 (equal ".." x)))
+           (directory-files dir
+                            nil))))
+
+(defvar debug-proc-cands nil "Nil.")
+
+(defun build-exec-list (string)
+  "Build a header of list containing STRING from CANDS."
+  (PDEBUG "ARGS" string debug-proc-cands)
+  (setq debug-proc-cands (cond
+               ((s-blank? string) debug-proc-cands)
+               ((s-ends-with? "/" string)
+                (PDEBUG "enter")
+                (yc/directory-files (if (s-starts-with? "/" string) string
+                                      (concat default-directory string))))
+               (t
+
+                (remove-if-not (lambda (x) (s-contains? string
+                                                        x)) debug-proc-cands)))))
+(defun counsel-find-exec (string)
+  "Filter header CANDS fiels for STRING."
+  (if (s-contains? " " string)
+      (let* ((array (split-string string " "))
+             (r-match-cand (s-join ".*?" array))
+             (new-cands (build-exec-list (car array))))
+        (PDEBUG "Format: " r-match-cand)
+        (remove-if-not
+         (lambda (x)
+           ;; filter func...
+           (string-match-p r-match-cand x))
+         new-cands))
+    (build-exec-list string)))
 
 (defun debug-proc (&optional proc)
   "Debug PROC.."
@@ -35,40 +70,11 @@
   (unless (executable-find "gdb")
     (error "Can't find executable gdb"))
 
-  (setq proc
-        (or proc
-            (let* ((cands (yc/directory-files default-directory)))
-              (defun build-exec-list (string)
-                "Build a header of list containing STRING from CANDS."
-                (PDEBUG "ARGS" string cands)
-                (setq cands (cond
-                             ((s-blank? string) cands)
-                             ((s-ends-with? "/" string)
-                              (PDEBUG "enter")
-                              (yc/directory-files (if (s-starts-with? "/" string) string
-                                                    (concat default-directory string))))
-                             (t
-
-                              (remove-if-not (lambda (x) (s-contains? string
-                                                                      x)) cands)))))
-
-              (defun counsel-find-exec (string)
-                "Filter header CANDS fiels for STRING."
-                (if (s-contains? " " string)
-                    (let* ((array (split-string string " "))
-                           (r-match-cand (s-join ".*?" array))
-                           (new-cands (build-exec-list (car array) cands)))
-                      (PDEBUG "Format: " r-match-cand)
-                      (remove-if-not
-                       (lambda (x)
-                         ;; filter func...
-                         (string-match-p r-match-cand x))
-                       new-cands))
-                  (build-exec-list string)))
-
-              (ivy-read "Name of process: "
-                        (lambda (string)
-                          (counsel-find-exec string))
+  (unless proc
+    (setq proc
+          (let* ((debug-proc-cands (yc/directory-files default-directory)))
+            (ivy-read "Name of process: "
+                      'counsel-find-exec
                         :initial-input ""
                         :dynamic-collection t
                         :unwind (lambda ()
@@ -89,7 +95,7 @@
   (unless (executable-find "gdb")
     (error "Can't find GNU debuger (gdb)"))
 
-  (let (pid cmd)
+  (let (pid)
     (when proc
       (if (numberp proc) (setq pid (number-to-string proc))
         (if (> (string-to-number proc) 0) (setq pid proc))))
@@ -99,7 +105,6 @@
                             (group (+ digit)) (+ space)
                             (group (+? ascii)) (+ space)
                             (+? ascii) eol))
-            (user (getenv "USER"))
             (ps_cmd
              (format "ps %s -o pid -o user -o start_time -o command | grep -v 'ps\\|grep'"
                      (format "-u %s" user-login-name)))
@@ -110,15 +115,12 @@
           (goto-char (point-min))
           (PDEBUG "PIDS:" (buffer-substring-no-properties (point-min) (point-max)))
           (while (search-forward-regexp ".+?$" nil t)
-            (add-to-list 'pid-list (match-string 0) t)))
+            (push (match-string 0)  pid-list)))
 
         (let ((choosen (ivy-read "Choose process: " pid-list :initial-input proc)) )
           (if (string-match r-match-entry choosen)
               (setq pid (match-string 1 choosen))
-            (error "Failed to parse PID"))
-          )
-        )
-      )
+            (error "Failed to parse PID")))))
 
     (unless pid
       (error "Can't find a running %s to attach" proc))
@@ -143,7 +145,7 @@
       (insert (shell-command-to-string ps-cmd))
       (goto-char (point-min))
       (while (search-forward-regexp ".+?$" nil t)
-        (add-to-list 'pid-list (match-string 0) t)))
+        (push (match-string 0) pid-list)))
 
     (PDEBUG "PID-LIST: " pid-list)
 
