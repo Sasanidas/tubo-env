371c98f59833517c1d74c5e24ee79d417ab098d6
yyc <yangyingchao@gmail.com>
2020-09-27 18:18:36 +0800

    remove cl-lib

--- .emacs.d/site-lisp/prog/lisp-utils.el
+++ .emacs.d/site-lisp/prog/lisp-utils.el
@@ -19,8 +19,7 @@
 ;;
 ;;; Code:
 
-(require 'cl-lib)
-
+(require 'dash)
 
 (defun call-stack ()
   "Return the current call stack frames."
@@ -29,21 +28,22 @@
         (index 5))
     (while (setq frame (backtrace-frame index))
       (push frame frames)
-      (incf index))
-    (remove-if-not 'car frames)))
+      (setq index (1+ index)))
+    (-filter 'car frames)))
 
 (defmacro compile-time-function-name ()
   "Get the name of calling function at expansion time."
   (symbol-name
    (cadadr
     (third
-     (find-if (lambda (frame) (ignore-errors (equal (car (third frame)) 'defalias)))
-              (reverse (call-stack)))))))
+     (car (-filter (lambda (frame) (ignore-errors (equal (car (third frame)) 'defalias)))
+              (reverse (call-stack))))))))
 
-;; (defun my-test-function ()
-;;   (message "This function is named '%s'" (compile-time-function-name)))
+(defun my-test-function ()
+  "Test function."
+  (interactive)
+  (message "This function is named '%s'" (compile-time-function-name)))
 
-;; (symbol-function 'my-test-function)
 ;; ;; you can see the function body contains the name, not a lookup
 ;; (lambda nil (message "This function is named '%s'" "my-test-function"))
 
